95697b0cd4064bedcb7f0a097e9c3fdb0114dccb
gkswns3708 <gkswns3708@naver.com>
2025-05-20 21:03:14 +0900

    Delete previous version directory

--- 2_Algorithms/24262.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-n = sys.stdin.readline()
-print(1)
-print(0)
--- 3_Arrays_and_Strings/108013.py
+++
@@ -1,9 +0,0 @@
-N, M = map(int, input().split())
-
-baskets = list(range(1, N + 1))
-
-for _ in range(M):
-    i, j = map(int, input().split()) 
-    baskets[i - 1], baskets[j - 1] = baskets[j - 1], baskets[i - 1]
-
-print(" ".join(map(str, baskets)))
