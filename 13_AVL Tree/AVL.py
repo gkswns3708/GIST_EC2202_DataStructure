e5fdd4425925355301d21cd214609366e02a1306
gkswns3708 <gkswns3708@naver.com>
2025-05-29 14:02:10 +0900

    Add Basic Implementation

--- 13_AVL Tree/AVL.py
+++ 13_AVL Tree/AVL.py
@@ -1,5 +1,7 @@
 from os import waitid
-from BST_Tree import BST
+from BST import BST
+
+
 class AVL(BST):
     def get_height(self, node):
         if node is None:
@@ -12,64 +14,63 @@ class AVL(BST):
         return self.get_height(node.left) - self.get_height(node.right)
 
     def insert(self, data):
-        node = super(AVL,self).insert(data)
+        node = super(AVL, self).insert(data)
         node.height = self.get_height(node)
         node = self.balancing(node)
         return node
 
     def delete(self, data):
-        node = super(AVL,self).deleteByMerging(data)
+        node = super(AVL, self).deleteByMerging(data)
         node = self.balancing(node)
         return node
 
-
     def rotate_right(self, node):
-      x = node.left
-      b = x.right
-      if node is None:
-        return None
-      if x is None:
-        return node
-      x.parent = node.parent
-      if node.parent is not None:
-        if node.parent.left == node:
-          node.parent.left = x
-        else:
-          node.parent.right = x
-      x.right = node
-      node.parent = x
-      node.left = b
-      if b is not None:
-        b.parent = node
-      if self.root == node:
-        self.root = x
-      node.height = self.get_height(node)
-      x.height = self.get_height(x)
-      return x
+        x = node.left
+        b = x.right
+        if node is None:
+            return None
+        if x is None:
+            return node
+        x.parent = node.parent
+        if node.parent is not None:
+            if node.parent.left == node:
+                node.parent.left = x
+            else:
+                node.parent.right = x
+        x.right = node
+        node.parent = x
+        node.left = b
+        if b is not None:
+            b.parent = node
+        if self.root == node:
+            self.root = x
+        node.height = self.get_height(node)
+        x.height = self.get_height(x)
+        return x
 
     def rotate_left(self, node):
-      x = node.right
-      b = x.left
-      if node is None:
-        return None
-      if x is None:
-        return node
-      x.parent = node.parent
-      if node.parent is not None:
-        if node.parent.left == node:
-          node.parent.left = x
-        else:
-          node.parent.right = x
-      x.left = node
-      node.parent = x
-      node.right = b
-      if b is not None:
-        b.parent = node
-      if self.root == node:
-        self.root = x
-      node.height = self.get_height(node)
-      x.height = self.get_height(x)
-      return x
+        x = node.right
+        b = x.left
+        if node is None:
+            return None
+        if x is None:
+            return node
+        x.parent = node.parent
+        if node.parent is not None:
+            if node.parent.left == node:
+                node.parent.left = x
+            else:
+                node.parent.right = x
+        x.left = node
+        node.parent = x
+        node.right = b
+        if b is not None:
+            b.parent = node
+        if self.root == node:
+            self.root = x
+        node.height = self.get_height(node)
+        x.height = self.get_height(x)
+        return x
 
     def rotate_right_left(self, node):
         self.rotate_right(node.right)
@@ -95,4 +96,3 @@ class AVL(BST):
             w = node
             node = node.parent
         return waitid
-
